ba16034329da3c0761c108b41fd12381f1b4b767
Atorami <atoramiseo@mail.ru>
2023-04-01 10:43:19 +0200

    feat: add NotFoundBlock

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
 import "./scss/app.scss";
 
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/Skeleton";
-
-import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://mocki.io/v1/306e8601-ef11-4ebe-91cf-1b86b23fc713")
-      .then((res) => {
-        return res.json();
-      })
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map(() => <Skeleton></Skeleton>)
-              : items.map((val) => <PizzaBlock key={val.id} {...val} />)}
-          </div>
+          <NotFound />
+          {/* <Home /> */}
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+
+export const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://mocki.io/v1/306e8601-ef11-4ebe-91cf-1b86b23fc713")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map(() => <Skeleton></Skeleton>)
+          : items.map((val) => <PizzaBlock key={val.id} {...val} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
