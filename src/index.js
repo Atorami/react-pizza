5956389e09ec7501a90bf987bea6cd6de46a2ae5
Atorami <atoramiseo@mail.ru>
2023-04-11 15:37:31 +0200

    feat: add Redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 
+import { store } from "./redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </Provider>
 );
