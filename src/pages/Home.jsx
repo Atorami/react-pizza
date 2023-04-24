041a69db33353e8bde70ca833eac0a35bf4eedff
Atorami <atoramiseo@mail.ru>
2023-04-24 14:47:46 +0200

    Add useNavigate from react-router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import qs from "qs";
 
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
@@ -15,6 +16,7 @@ import { searchContext } from "../App";
 import { setCategoryId, setCurrentPage } from "../redux/slices/filterSlice";
 
 export const Home = () => {
+  const navigate = useNavigate();
   const categoryId = useSelector((state) => state.filter.categoryId);
   const sortType = useSelector((state) => state.filter.sort.sortProperty);
   const currentPage = useSelector((state) => state.filter.currentPage);
@@ -34,7 +36,7 @@ export const Home = () => {
 
   useEffect(() => {
     const queryString = qs.stringify({
-      sortProperty: sort.sortProperty,
+      sortProperty: sortType,
       categoryId,
       currentPage,
     });
@@ -61,6 +63,15 @@ export const Home = () => {
       });
     window.scrollTo(0, 0);
   }, [categoryId, sortType, searchValue, currentPage]);
+
+  useEffect(() => {
+    const queryString = qs.stringify({
+      sortType,
+      categoryId,
+      currentPage,
+    });
+    navigate(`?${queryString}`);
+  }, [categoryId, sortType, currentPage]);
 
   return (
     <div className="container">
